0ad5b64eb2d9437f554a25ad3ca11f35756462dc
Alexander Shuhin <a.medium@gmail.com>
2011-03-24 15:54:31 +0300

    Continued work on new version of extension

--- app/controllers/compare_products_controller.rb
+++ app/controllers/compare_products_controller.rb
@@ -1,78 +1,75 @@
 class CompareProductsController < Spree::BaseController
-  
+  COMPARE_LIMIT = 4
   before_filter :find_taxon, :only => [:show, :add]
-  #before_filter :verify_comparable_taxon, :only => :show
-  
+
   helper :products, :taxons
-  
+
   def show
-    @properties = @products.map(&:properties).flatten.uniq # We return the list of properties here so we can use them latter.
+    if @comparable_products.count > 1
+      @properties = @comparable_products.includes(:product_properties => :property).map(&:properties).flatten.uniq # We return the list of properties here so we can use them latter.
+    else
+      flash[:error] = I18n.t(:insufficient_data, :scope => :compare_products)
+    end
   end
-  
+
   def add
     product = Product.find_by_permalink(params[:id])
-    if product && (product.taxons.map(&:id).include?(@taxon.try(:id)) || @products.size < 1)
-      session[:comparable_product_ids] << product.id
-      session[:comparable_product_ids].uniq!
-      flash[:notice] = I18n.t(:added_to_comparison, :scope => :compare_products)
+    if product && (product.taxons.map(&:id).include?(@taxon.try(:id)) || @comparable_products.size < 1)
+      if session[:comparable_product_ids].include?(product.id)
+        flash[:notice] = I18n.t(:already_in_list, :product => product.name, :scope => :compare_products)
+      else
+        if @comparable_products.size < COMPARE_LIMIT
+          @added_product = product
+          session[:comparable_product_ids] << product.id
+          flash[:notice] = I18n.t(:added_to_comparsion, :product => product.name, :scope => :compare_products)
+        else
+          flash[:notice] = I18n.t(:limit_is_reached, :scope => :compare_products, :count => COMPARE_LIMIT)
+        end
+      end
     else
-      session[:comparable_product_ids].delete(product.id) 
+      session[:comparable_product_ids].delete(product.id)
       flash[:error] = I18n.t(:invalid_taxon, :scope => :compare_products)
     end
     respond_to do |format|
       format.html { redirect_back_or_default(products_path) }
-      format.js { render :layout => false }  
+      format.js { render :layout => false }
     end
   end
-  
+
   def remove
     session[:comparable_product_ids] ||= []
     product = Product.find_by_permalink(params[:id])
     if product
+      @deleted_product = product if session[:comparable_product_ids].include?(product.id)
       session[:comparable_product_ids].delete(product.id)
-      flash[:notice] = I18n.t(:removed_from_comparison, :product => product.name, :scope => :compare_products)
+      flash[:notice] = I18n.t(:removed_from_comparsion, :product => product.name, :scope => :compare_products)
     end
     respond_to do |format|
       format.html { redirect_back_or_default(products_path) }
-      format.js { render :layout => false }  
+      format.js { render :layout => false }
     end
   end
-  
+
   def destroy
     session[:comparable_product_ids] = []
-    flash[:notice] = I18n.t(:comparison_cleared, :scope => :compare_products)
+    flash[:notice] = I18n.t(:comparsion_cleared, :scope => :compare_products)
     respond_to do |format|
       format.html { redirect_back_or_default(products_path) }
-      format.js { render :layout => false }  
+      format.js { render :layout => false }
     end
   end
-  
+
   private
-  
+
   def find_taxon
-    @products = find_products
-    if @products.size > 1
-      @taxon = @products.inject(@products[0].taxons){|t, p| t & p.taxons }.select{|t| t.is_comparable? }.first
+    if @comparable_products.size > 1
+      @taxon = @comparable_products.inject(@comparable_products[0].taxons) { |t, p| t & p.taxons }.select { |t| t.is_comparable? }.first
 
       if @taxon.nil?
-        flash[:error] = I18n.t('compare_products.invalid_taxon')
+        flash[:error] = I18n.t(:invalid_taxon, :scope => :compare_products)
       end
-    elsif @products.size == 1
-      @taxon = @products[0].taxons.select{|t| t.is_comparable? }.first
+    elsif @comparable_products.size == 1
+      @taxon = @comparable_products[0].taxons.select { |t| t.is_comparable? }.first
     end
   end
-  
-  
-  def find_products
-    session[:comparable_product_ids] ||= []
-    product_ids = session[:comparable_product_ids]
-    if product_ids.length >= 4
-      flash[:notice] = I18n.t('compare_products.limit_is_4')
-      product_ids = product_ids[0...3]
-    elsif product_ids.length < 2
-      flash[:error] = I18n.t('compare_products.insufficient_data')
-    end
-    Product.find(:all, :conditions => { :id => product_ids}, :include => { :product_properties => :property }, :limit => 4)
-  end
-  
 end
--- lib/spree_compare_products.rb
+++ lib/spree_compare_products.rb
@@ -4,27 +4,54 @@ require 'spree_compare_products/comparable_container'
 
 module SpreeCompareProducts
   class Engine < Rails::Engine
-    
+
     config.autoload_paths += %W(#{config.root}/lib)
-    
+
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../app/**/*_decorator*.rb')) do |c|
         Rails.env.production? ? require(c) : load(c)
       end
-      
+
       Taxonomy.class_eval do
         include SpreeCompareProducts::ComparableContainer
       end
-      
+
       Taxon.class_eval do
         include SpreeCompareProducts::ComparableContainer
-        
+
         def is_comparable?
           comparable? || taxonomy.comparable?
         end
       end
     end
-    
+
     config.to_prepare &method(:activate).to_proc
   end
 end
+
+module SpreeBase
+  module InstanceMethods
+    protected
+
+    def find_comparable_products
+      session[:comparable_product_ids] ||= []
+      product_ids = session[:comparable_product_ids]
+      if product_ids.size > 0
+        @comparable_products = Product.where(:id => product_ids)
+      else
+        @comparable_products = []
+      end
+    end
+  end
+
+  class << self
+
+    def included_with_compare_products(receiver)
+      included_without_compare_products(receiver)
+      receiver.send :before_filter, 'find_comparable_products'
+    end
+
+    alias_method_chain :included, :compare_products
+  end
+end
+
