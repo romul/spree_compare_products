e98da2e588d62f63868a19c42a6e8e93ec507733
Alexander Shuhin <a.medium@gmail.com>
2011-03-24 15:54:31 +0300

    Continued work on new version of extension

--- lib/spree_compare_products_hooks.rb
+++ lib/spree_compare_products_hooks.rb
@@ -3,21 +3,13 @@ class SpreeCompareProductsHooks < Spree::ThemeSupport::HookListener
   insert_after :admin_inside_taxonomy_form, 'admin/taxonomies/comparable_field'
   
   replace :taxon_products, 'taxons/taxon_products'
+
+  #insert_after :products_list_item, 'shared/compare_product_field'
   
-  insert_after :products_list_item, 'shared/compare_product_field'
+  insert_after :shared_login_bar, 'shared/comparable_products'
   
-  insert_after :cart_form, 'products/compare'
-  
-  insert_after :sidebar do 
-    %(
-    <div class="sidebar-item" id="comparable_products_wrapper">
-      <%= render 'shared/comparable_products' %>
-    </div>
-    )
-  end
-  
-  insert_after :inside_head do
-    "<%= javascript_include_tag 'product_comparison' %>"
-  end
+  insert_after :inside_head, 'shared/compare_product_javascript'
+
+  #insert_after :cart_form, 'products/compare'
   
 end
